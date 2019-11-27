1d052530f8495672f3896fb9c19621111278a1d4
Salazar-prime <aggarw82@purdue.edu>
2019-11-27 17:39:01 -0500

    start script

---
+++ start.sh
@@ -0,0 +1,13 @@
+# cd /home/salazar_prime007/work/Shallow_learn/HW3
+clear
+pwd
+source activate /home/salazar/anaconda3/envs/incepv4
+echo -e "\n\e[31m____________________________GRAPH______________________________________\e[0m\n"
+git graph -n 5
+echo -e "\n\e[31m____________________________STATUS_____________________________________\e[0m\n"
+git st
+echo -e "\n\e[31m____________________________START JUPYTER______________________________\e[0m\n"
+jupyter notebook
+echo -e "\n\e[31m____________________[  BYE. DONT FORGET TO GIT ]_______________________\e[0m\n"
+git st
+#cd ~
